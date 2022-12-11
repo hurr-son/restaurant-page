08fbb94d11fa13ddac2d88dbf88842c521378cea
hurr-son <hprewett05@gmail.com>
2022-12-10 23:05:40 -0600

    this file is for rendering dom els

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './style.css';
+import { header, welcome} from './initial-comp';
+
+import image from './images/milkyway.jpg';
+
+
+const initLoad = (function() {
+    
+    const content = document.getElementById('content');
+    
+    const coverImg = new Image();
+    coverImg.src = image
+    coverImg.classList.add('cover-img')
+    content.appendChild(coverImg)
+
+
+    content.appendChild(header())
+    content.appendChild(welcome())
+
+})()
+
+
 
 
