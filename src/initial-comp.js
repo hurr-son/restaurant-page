1ea6555216f0f78468bd0bd9f258fdb25d638712
hurr-son <hprewett05@gmail.com>
2022-12-10 23:02:57 -0600

    this file for creation and exportation of dom els messed up on the last commit message

---
+++ src/initial-comp.js
@@ -0,0 +1,37 @@
+
+
+function header(){
+    const headerWrapEl = document.createElement('div');
+    headerWrapEl.classList.add('header-wrapper');
+    const headerEl = document.createElement('h1');
+    headerEl.innerText = 'Next to the Wormhole';
+    headerEl.classList.add('res-title');
+    headerWrapEl.appendChild(headerEl)
+    return headerWrapEl;
+}
+
+
+
+function welcome() {
+    const welcomeWrapEl = document.createElement('div');
+    welcomeWrapEl.classList.add('welcome-wrapper');
+    const welcomeEl = document.createElement('p');
+    
+    welcomeEl.innerText = 'Welcome to Next to the Wormhole! We are a restaurant founded by space explorers who returned from a journey through a nearby wormhole. Our menu features unique dishes created using ingredients and cooking techniques from various planets and dimensions. We are a popular destination for both locals and intergalactic travelers alike. We hope you enjoy your dining experience with us!';
+
+    welcomeEl.classList.add('welcome-message');
+    
+    welcomeWrapEl.appendChild(welcomeEl);
+    return welcomeWrapEl;
+
+}
+
+function hours() {
+
+}
+
+function location() {
+
+}
+
+export {header, welcome, hours, location};
