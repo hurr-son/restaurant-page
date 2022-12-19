1a6b422a5292548779c5afcf5e7925666cf83abd
hurr-son <hprewett05@gmail.com>
2022-12-19 09:38:16 -0600

    created homeload func for the homeEls
    also created load functions for menu page and
    contact page

--- src/page-load.js
+++ src/page-load.js
@@ -1,4 +1,4 @@
-import { header, welcome, hours, location} from './home-comp';
+import { createNavbar, createWelcome, createHours, createLocation} from './home-comp';
 
 import image from './images/milkyway.jpg';
 
@@ -8,17 +8,22 @@ function initLoad() {
 const content = document.getElementById('content');
 
 const coverImg = new Image();
-coverImg.src = image
-coverImg.classList.add('cover-img')
-content.appendChild(coverImg)
-
-
-content.appendChild(header())
-content.appendChild(welcome())
-content.appendChild(hours())
-content.appendChild(location())
+coverImg.src = image;
+coverImg.classList.add('cover-img');
+content.appendChild(coverImg);
+content.appendChild(createNavbar());
 
 }
 
 
-export { initLoad };
+function homeLoad() {
+    const content = document.getElementById('content');
+    content.appendChild(createWelcome());
+    content.appendChild(createHours());
+    content.appendChild(createLocation());
+}
+
+
+
+
+export { initLoad, homeLoad};
