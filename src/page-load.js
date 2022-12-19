784735809b0cd71d4f4492fedd485b9c141bac57
hurr-son <hprewett05@gmail.com>
2022-12-18 20:08:42 -0600

    transfered initLoad into correct js module

---
+++ src/page-load.js
@@ -0,0 +1,24 @@
+import { header, welcome, hours, location} from './initial-comp';
+
+import image from './images/milkyway.jpg';
+
+
+function initLoad() {
+    
+const content = document.getElementById('content');
+
+const coverImg = new Image();
+coverImg.src = image
+coverImg.classList.add('cover-img')
+content.appendChild(coverImg)
+
+
+content.appendChild(header())
+content.appendChild(welcome())
+content.appendChild(hours())
+content.appendChild(location())
+
+}
+
+
+export {initLoad};
