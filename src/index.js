9fdf7a799059b36f829bc115d8d96611f938ebbd
hurr-son <hprewett05@gmail.com>
2022-12-22 14:00:14 -0600

    built tabbing logic with eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
 import './style.css';
-import { homeLoad, initLoad, menuLoad, contactLoad } from './page-load';
+import { initialEls } from './page-load';
+import { createHomeWrapEl } from './home-comp';
+import { createMenuWrapEl } from './menu-comp';
+import { createContWrapEl } from './contact-comp';
 
-initLoad()
-homeLoad()
-menuLoad()
-contactLoad()
+const content = document.getElementById('content');
+const homeEls = createHomeWrapEl()
+content.appendChild(initialEls());
+content.appendChild(homeEls);
+
+const menuEls = createMenuWrapEl();
+const contactEls = createContWrapEl();
+
+function renderHomeOnClick(e) {
+    if(!e.target.classList.contains('home')) {
+        return
+    }
+        let currentWrap = document.querySelector('.wrapper');   
+        content.replaceChild(homeEls, currentWrap);
+}
+
+function renderMenuOnClick(e) {
+    if(!e.target.classList.contains('menu')) {
+        return
+    }
+        let currentWrap = document.querySelector('.wrapper');
+        content.replaceChild(menuEls, currentWrap);
+}
+    
+function renderContactOnClick(e) {
+    if(!e.target.classList.contains('contact')) {
+        return
+    }
+        let currentWrap = document.querySelector('.wrapper');
+        content.replaceChild(contactEls, currentWrap);
+}
+
+content.addEventListener('click', renderHomeOnClick);
+content.addEventListener('click', renderMenuOnClick);
+content.addEventListener('click', renderContactOnClick);
+
+
+
+
+
 
 
